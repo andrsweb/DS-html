3ed124c366be52bfe0f6acb1bd2977975e5084d9
andrsweb <d3vv.proo@gmail.com>
2026-01-13 06:54:46 +0700

    create slider

---
+++ dev/src/js/thumbs-slider.js
@@ -0,0 +1,52 @@
+import Swiper from 'swiper';
+import {Navigation, Thumbs, Pagination} from 'swiper/modules';
+
+document.addEventListener('DOMContentLoaded', () => {
+	'use strict';
+
+	initSwiperThumbs();
+});
+
+const initSwiperThumbs = () => {
+	const thumbsSwiper = new Swiper('.thumbs-swiper-container', {
+		modules: [Thumbs],
+		slidesPerView: 2.3,
+		spaceBetween: 10,
+		watchSlidesProgress: true,
+		breakpoints: {
+			420: {
+				slidesPerView: 2.7
+			},
+			650: {
+				slidesPerView: 3.5
+			},
+			768: {
+				slidesPerView: 4.5
+			},
+			992: {
+				slidesPerView: 5.5
+			},
+			1200: {
+				slidesPerView: 7
+			}
+		}
+	});
+
+	new Swiper('.main-swiper', {
+		modules: [Navigation, Thumbs, Pagination],
+		slidesPerView: 1,
+		spaceBetween: 16,
+		navigation: {
+			nextEl: '.swiper-next',
+			prevEl: '.swiper-prev',
+		},
+		thumbs: {
+			swiper: thumbsSwiper,
+		},
+		pagination: {
+			el: '.swiper-pagination',
+			dynamicBullets: true,
+			clickable: true,
+		},
+	});
+};
